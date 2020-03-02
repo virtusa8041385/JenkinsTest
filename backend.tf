0990879b0befdef34687db27d84c5901151b4c65
virtusa8041385 <35696897+virtusa8041385@users.noreply.github.com>
2020-03-02 10:55:47 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform{
   backend "remote"{
     hostname = "app.terraform.io"
-    organiaztion  = "JenkinsTest"
+    organization  = "JenkinsTest"
     workspaces{
       name = "JenkinsTest"
     }
